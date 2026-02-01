f7b7268e59f90e57875f42cd2d860f4f2b427207
DrTMUSHACoder <dr.tmushacoder@gmail.com>
2026-02-01 10:21:28 +0530

    fix: remove base path to rely on default vite auto-detection

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // Safe config for Netlify
 export default defineConfig({
     plugins: [react()],
-    base: './',
     build: {
         outDir: 'dist',
         sourcemap: true
